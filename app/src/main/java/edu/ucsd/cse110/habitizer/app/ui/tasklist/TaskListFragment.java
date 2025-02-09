9411d0087ff31b60f30b1f0f789e30ecb69819d5
miney <miney@ucsd>
2025-02-08 22:40:03 -0800

    Finished US1. Added ListView for Tasks.

---
+++ app/src/main/java/edu/ucsd/cse110/habitizer/app/ui/tasklist/TaskListFragment.java
@@ -0,0 +1,74 @@
+package edu.ucsd.cse110.habitizer.app.ui.tasklist;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.ucsd.cse110.habitizer.app.MainViewModel;
+import edu.ucsd.cse110.habitizer.app.MainViewModel;
+import edu.ucsd.cse110.habitizer.app.databinding.FragmentTaskListBinding;
+
+
+public class TaskListFragment extends Fragment {
+    private MainViewModel activityModel;
+    private FragmentTaskListBinding view;
+    private TaskListAdapter adapter;
+
+    public TaskListFragment() {
+        // Required empty public constructor
+    }
+
+    public static TaskListFragment newInstance() {
+        TaskListFragment fragment = new TaskListFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Initialize the Model
+        var modelOwner = requireActivity();
+        var modelFactory = ViewModelProvider.Factory.from(MainViewModel.initializer);
+        var modelProvider = new ViewModelProvider(modelOwner, modelFactory);
+        this.activityModel = modelProvider.get(MainViewModel.class);
+
+        // Initialize the Adapter (with an empty list for now)
+        this.adapter = new TaskListAdapter(requireContext(), List.of());
+
+        activityModel.getOrderedTasks().observe(tasks -> {
+            if (tasks == null) return;
+            adapter.clear();
+            adapter.addAll(new ArrayList<>(tasks)); // remember the mutable copy here!
+            adapter.notifyDataSetChanged();
+        });
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        this.view = FragmentTaskListBinding.inflate(inflater, container, false);
+
+        // Set the adapter on the ListView
+        view.taskList.setAdapter(adapter);
+
+        // show CreateTaskDialogFragement
+//        view.createTaskButton.setOnClickListener(v -> {
+//            var dialogFragment = CreateTaskDialogFragment.newInstance();
+//            dialogFragment.show(getParentFragmentManager(), "CreateTaskDialogFragment");
+//        });
+
+        return view.getRoot();
+    }
+}
