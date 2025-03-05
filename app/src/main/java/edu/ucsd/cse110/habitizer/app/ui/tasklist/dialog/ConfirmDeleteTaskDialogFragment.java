89e337e2cea2a2c36c11e872104bea9d39607c0c
Chris <christopherrebollar0@icloud.com>
2025-03-04 16:34:48 -0800

    Update ConfirmDeleteTaskDialogFragment.java
    added debugging log import

--- app/src/main/java/edu/ucsd/cse110/habitizer/app/ui/tasklist/dialog/ConfirmDeleteTaskDialogFragment.java
+++ app/src/main/java/edu/ucsd/cse110/habitizer/app/ui/tasklist/dialog/ConfirmDeleteTaskDialogFragment.java
@@ -3,6 +3,7 @@ import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
